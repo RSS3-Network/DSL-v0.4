15a5d35f1198af7a168ef3cadef823ab1051e9a7
Zerber <zerberrabbit@gmail.com>
2022-06-10 22:05:15 +0800

    feat: add GetMultipleSpaces

--- common/snapshot/client.go
+++ common/snapshot/client.go
@@ -1,6 +1,8 @@
 package snapshot
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/shurcooL/graphql"
@@ -12,17 +14,74 @@ const (
 	EndpointPath   = "/graphql"
 )
 
+type Description string
+
+const (
+	DescriptionAsc  Description = "asc"
+	DescriptionDesc Description = "desc"
+)
+
 type Client struct {
 	httpClient    *http.Client
 	graphqlClient *graphql.Client
 }
 
-func (c *Client) GetMultipleSpaces() {
+type GetMultipleSpacesVariable struct {
+	First          graphql.Int
+	Skip           graphql.Int
+	OrderBy        graphql.String
+	OrderDirection Description
+}
 
+func (c *Client) GetMultipleSpaces(ctx context.Context, variable GetMultipleSpacesVariable) error {
+	variableMap := map[string]interface{}{}
+
+	if variable.First > 0 {
+		variableMap["first"] = variable.First
+	} else {
+		return fmt.Errorf("variable 'First' must be greater than 0")
+	}
+
+	if variable.Skip >= 0 {
+		variableMap["first"] = variable.First
+	} else {
+		return fmt.Errorf("variable 'Skip' must be greater than 0")
+	}
+
+	if variable.OrderBy != "" {
+		variableMap["first"] = variable.First
+	} else {
+		return fmt.Errorf("variable 'OrderBy' must not be nil")
+	}
+
+	variableMap["OrderDirection"] = variable.OrderDirection
+
+	if err := c.graphqlClient.Query(ctx, query, variableMap); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+type GetMultipleProposalsVariable struct {
+	First          graphql.Int
+	Skip           graphql.Int
+	WhereSpaceIn   []graphql.String
+	WhereState     graphql.String
+	orderBy        graphql.String
+	orderDirection Description
 }
 
 func (c *Client) GetMultipleProposals(name string) {
 
+}
+
+type GetMultipleVotesVariable struct {
+	First           graphql.Int
+	Skip            graphql.Int
+	WhereProposalIn []graphql.String
+	orderBy         graphql.String
+	orderDirection  Description
 }
 
 func (c *Client) GetMultipleVotes() {
