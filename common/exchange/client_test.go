231254659e99cb5c79bc62b8cd3a86296fc6702b
Kind Jeff <kindjeff.com@gmail.com>
2022-07-07 16:11:20 +0800

    fix: exchange_test run forever (#99)

--- common/exchange/client_test.go
+++ common/exchange/client_test.go
@@ -17,13 +17,22 @@ func init() {
 
 // this test might take a while to run (>1 minutes)
 func TestGetSwapPools(t *testing.T) {
+	errs := make(chan error, 100)
+
 	lop.ForEach(exchange.SwapProviders, func(provider exchange.SwapProvider, k int) {
 		lop.ForEach(provider.SwapPools, func(pool exchange.SwapPool, i int) {
 			result, err := client.GetSwapPairs(context.Background(), provider.Name, pool)
 			if err != nil {
-				t.Fatal(err)
+				t.Log(err)
+				errs <- err
 			}
 			t.Log(len(result))
 		})
 	})
+
+	if len(errs) != 0 {
+		for err := range errs {
+			t.Fatal(err)
+		}
+	}
 }
