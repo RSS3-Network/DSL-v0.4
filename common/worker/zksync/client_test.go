acda27e8798b76836ffa1f76e45edcf68bf4d07f
KallyDev <kallydev@gmail.com>
2022-07-19 14:44:56 +0800

    feat(common): support get token list from zksync

---
+++ common/worker/zksync/client_test.go
@@ -0,0 +1,21 @@
+package zksync_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/naturalselectionlabs/pregod/common/worker/zksync"
+)
+
+var client = zksync.New()
+
+func TestGetTokenList(t *testing.T) {
+	tokenList, err := client.GetTokenList(context.Background())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for _, token := range tokenList {
+		t.Log(token.ID, token.Address, token.Symbol, token.Kind, token.IsNFT)
+	}
+}
