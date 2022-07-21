f8c2b115b67628d9b0ec9d54c76dfcadebe8a8e3
KallyDev <kallydev@gmail.com>
2022-07-21 13:58:48 +0800

    style: golang ci lint

--- common/datasource/ethereum/contract/crossbell/constant.go
+++ common/datasource/ethereum/contract/crossbell/constant.go
@@ -4,6 +4,4 @@ import "github.com/ethereum/go-ethereum/common"
 
 // https://scan.crossbell.io/address/0xa6f969045641Cf486a747A2688F3a5A6d43cd0D8
 
-var (
-	AddressCharacter = common.HexToAddress("0xa6f969045641Cf486a747A2688F3a5A6d43cd0D8")
-)
+var AddressCharacter = common.HexToAddress("0xa6f969045641Cf486a747A2688F3a5A6d43cd0D8")
--- common/datasource/ethereum/contract/lens/constant.go
+++ common/datasource/ethereum/contract/lens/constant.go
@@ -4,6 +4,4 @@ import "github.com/ethereum/go-ethereum/common"
 
 // https://docs.lens.xyz/docs/deployed-contract-addresses
 
-var (
-	AddressHub = common.HexToAddress("0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d")
-)
+var AddressHub = common.HexToAddress("0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d")
