2dace54aebb4afb3a85d6948711427dbcfcbbc07
KallyDev <kallydev@gmail.com>
2022-07-21 13:52:07 +0800

    feat(indexer/middleware): profile resolver for lens and corssbell

---
+++ common/datasource/ethereum/contract/lens/constant.go
@@ -0,0 +1,9 @@
+package lens
+
+import "github.com/ethereum/go-ethereum/common"
+
+// https://docs.lens.xyz/docs/deployed-contract-addresses
+
+var (
+	AddressHub = common.HexToAddress("0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d")
+)
