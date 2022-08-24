1cd5407a517364a6ce5a915f70b9024b0c50f986
polebug <polebugfly@gmail.com>
2022-08-24 16:07:04 +0800

    update(indexer): add address indexer status (#155)

--- common/database/model/address.go
+++ common/database/model/address.go
@@ -1,6 +1,10 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/lib/pq"
+)
 
 const (
 	AddressStatusDone    = "done"
@@ -8,9 +12,12 @@ const (
 )
 
 type Address struct {
-	Address   string    `gorm:"column:address;primaryKey" json:"address"`
-	CreatedAt time.Time `gorm:"column:created_at;autoCreateTime;not null;default:now();index" json:"-"`
-	UpdatedAt time.Time `gorm:"column:updated_at;autoUpdateTime;not null;default:now();index" json:"updated_at"`
+	Address          string         `gorm:"column:address;primaryKey" json:"address"`
+	Status           bool           `gorm:"column:status" json:"status"`
+	DoneNetworks     pq.StringArray `gorm:"column:done_networks;type:text[]" json:"done_networks"`
+	IndexingNetworks pq.StringArray `gorm:"column:indexing_networks;type:text[]" json:"indexing_networks"`
+	CreatedAt        time.Time      `gorm:"column:created_at;autoCreateTime;not null;default:now();index" json:"-"`
+	UpdatedAt        time.Time      `gorm:"column:updated_at;autoUpdateTime;not null;default:now();index" json:"updated_at"`
 }
 
 func (Address) TableName() string {
