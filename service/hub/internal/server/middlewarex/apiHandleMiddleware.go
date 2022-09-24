9cbb8e90af3f205a58886ad2e9014f7dfd3919fa
Henry Wang <henry@rss3.io>
2022-09-24 17:17:29 +0100

    feat(hub): support unstoppable domains NS (#196)
    * feat(hub): support unstoppable domains NS
    
    * chore: rename func

--- service/hub/internal/server/middlewarex/apiHandleMiddleware.go
+++ service/hub/internal/server/middlewarex/apiHandleMiddleware.go
@@ -7,7 +7,6 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/naturalselectionlabs/pregod/common/database"
 	"github.com/naturalselectionlabs/pregod/common/database/model"
-	"github.com/naturalselectionlabs/pregod/common/worker/ens"
 	"github.com/naturalselectionlabs/pregod/service/hub/internal/server/handler"
 )
 
@@ -15,7 +14,7 @@ func APIMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		address := c.Param("address")
 		if address != "" {
-			if address, err := TranslateAddress(address); err != nil {
+			if address, err := ResolveAddress(address); err != nil {
 				return err
 			} else {
 				c.SetParamValues(address)
@@ -67,37 +66,9 @@ func CheckAPIKey(apiKey string) error {
 	return nil
 }
 
-// TranslateAddress translate handles into an address
-func TranslateAddress(address string) (string, error) {
-	result := address
+// ResolveAddress resolve handles into an address
+func ResolveAddress(address string) (string, error) {
+	result := handler.ReverseResolveAll(strings.ToLower(address), false)
 
-	splits := strings.Split(address, ".")
-
-	if len(splits) < 2 {
-		return strings.ToLower(result), nil
-	}
-
-	switch splits[len(splits)-1] {
-	case "eth":
-		nsResult, err := ens.Resolve(address)
-		if err != nil {
-			return "", fmt.Errorf("failed to resolve ENS address %s: %s", address, err)
-		}
-
-		result = nsResult
-	case "csb":
-		nsResult, err := handler.ResolveCrossbell(address)
-		if err != nil {
-			return "", fmt.Errorf("failed to resolve Crossbell address %s: %s", address, err)
-		}
-		result = nsResult
-	case "lens":
-		nsResult, err := handler.ResolveLens(address)
-		if err != nil {
-			return "", fmt.Errorf("failed to resolve Lens address %s: %s", address, err)
-		}
-		result = nsResult
-	}
-
-	return strings.ToLower(result), nil
+	return strings.ToLower(result.Address), nil
 }
