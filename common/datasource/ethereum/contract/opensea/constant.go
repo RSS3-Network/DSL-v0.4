f40873cbafbdd8c9da30f61fd6647b38badaccb8
KallyDev <36319157+kallydev@users.noreply.github.com>
2022-10-20 16:37:14 +0800

    fix(worker/marketplace): failure to parse  wyvern exchange transactions (#218)
    * perf: remove unnecessary rpc requests
    
    * fix: missing some nft trade transactions
    
    * fix: confusion between nft and erc20 contract addresses
    
    * fix: lost scan url and display value field
    
    * style: add some comments

--- common/datasource/ethereum/contract/opensea/constant.go
+++ common/datasource/ethereum/contract/opensea/constant.go
@@ -5,10 +5,16 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
+const (
+	ItemTypeERC20 uint8 = 1
+
+	Platform = "OpenSea"
+)
+
 var (
-	AddressSeaport = common.HexToAddress("0x00000000006c3852cbEf3e08E8dF289169EdE581")
+	AddressSeaport        = common.HexToAddress("0x00000000006c3852cbEf3e08E8dF289169EdE581")
+	AddressWyvernExchange = common.HexToAddress("0x7Be8076f4EA4A4AD08075C2508e481d6C946D12b")
 
-	EventHashOrderFulfilled = common.BytesToHash(crypto.Keccak256([]byte("OrderFulfilled(bytes32,address,address,address,(uint8,address,uint256,uint256)[],(uint8,address,uint256,uint256,address)[])")))
-
-	PlatformOpenSea = "OpenSea"
+	EventHashOrderFulfilled = crypto.Keccak256Hash([]byte("OrderFulfilled(bytes32,address,address,address,(uint8,address,uint256,uint256)[],(uint8,address,uint256,uint256,address)[])"))
+	EventHashOrdersMatched  = crypto.Keccak256Hash([]byte("OrdersMatched(bytes32,bytes32,address,address,uint256,bytes32)"))
 )
