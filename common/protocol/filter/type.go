cfdc8196760b90a84b7109ee9e891fc6ded5a215
KallyDev <36319157+kallydev@users.noreply.github.com>
2022-10-25 12:45:00 +0800

    feat(worker/crossbell): mint and comment note (#226)
    * feat: support mint note event
    
    * feat: support comment note
    
    * style: fix a typo
    
    * fix: missing author field
    
    * style: replace handle to url
    
    * fix: wrong type_on_platform field value

--- common/protocol/filter/type.go
+++ common/protocol/filter/type.go
@@ -37,6 +37,7 @@ const (
 	SocialFollow   string = "follow"
 	SocialUnfollow string = "unfollow"
 	SocialLike     string = "like"
+	SocialMint     string = "mint"
 
 	// sub-types for Social-Profile
 	SocialProfileCreate string = "create"
@@ -71,5 +72,6 @@ func CheckTypeValid(tag string, transferType string) bool {
 			return true
 		}
 	}
+
 	return false
 }
