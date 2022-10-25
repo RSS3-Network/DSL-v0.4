6434e6a69e87302468b1c0eff859a443ea81e408
polebug <polebugfly@gmail.com>
2022-10-25 14:32:02 +0800

    update(hub): update name service api

--- service/hub/internal/server/handler/handler_name_service.go
+++ service/hub/internal/server/handler/handler_name_service.go
@@ -33,9 +33,7 @@ func (h *Handler) GetNameResolveFunc(c echo.Context) error {
 
 	go h.filterReport(model.GetNS, request)
 
-	// temporary disable ReverseResolveAll
-	// result := name_service.ReverseResolveAll(strings.ToLower(request.Address), true)
-	result := name_service.ReverseResolveAll(strings.ToLower(request.Address), false)
+	result := name_service.ReverseResolveAll(strings.ToLower(request.Address), true)
 
 	if len(result.Address) == 0 {
 		return AddressIsInvalid(c)
