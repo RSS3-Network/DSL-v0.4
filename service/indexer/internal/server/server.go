d99c0148ea71b0f3ffd5840b2a4d587e2caad099
Henry Wang <henry@rss3.io>
2022-11-10 11:45:52 +0000

    del: Lens indexer/worker and datasource (#251)

--- service/indexer/internal/server/server.go
+++ service/indexer/internal/server/server.go
@@ -34,7 +34,6 @@ import (
 	"github.com/naturalselectionlabs/pregod/service/indexer/internal/datasource/eip1577"
 	"github.com/naturalselectionlabs/pregod/service/indexer/internal/datasource/moralis"
 	eth_etl "github.com/naturalselectionlabs/pregod/service/indexer/internal/datasource/pregod_etl/ethereum"
-	"github.com/naturalselectionlabs/pregod/service/indexer/internal/datasource/pregod_etl/lens"
 	"github.com/naturalselectionlabs/pregod/service/indexer/internal/datasource/zksync"
 	"github.com/naturalselectionlabs/pregod/service/indexer/internal/datasource_asset"
 	alchemy_asset "github.com/naturalselectionlabs/pregod/service/indexer/internal/datasource_asset/alchemy"
@@ -47,7 +46,6 @@ import (
 	"github.com/naturalselectionlabs/pregod/service/indexer/internal/worker/exchange/swap"
 	"github.com/naturalselectionlabs/pregod/service/indexer/internal/worker/governance/snapshot"
 	"github.com/naturalselectionlabs/pregod/service/indexer/internal/worker/social/crossbell"
-	lens_worker "github.com/naturalselectionlabs/pregod/service/indexer/internal/worker/social/lens"
 	"github.com/naturalselectionlabs/pregod/service/indexer/internal/worker/social/mirror"
 	"github.com/naturalselectionlabs/pregod/service/indexer/internal/worker/transaction"
 	"github.com/naturalselectionlabs/pregod/service/indexer/internal/worker/transaction/bridge"
@@ -133,18 +131,12 @@ func (s *Server) Initialize() (err error) {
 		return err
 	}
 
-	lensDatasource, err := lens.New(s.config.RPC)
-	if err != nil {
-		return err
-	}
-
 	s.datasources = []datasource.Datasource{
 		alchemyDatasource,
 		moralis.New(s.config.Moralis.Key),
 		arweave.New(),
 		blockscoutDatasource,
 		zksync.New(),
-		lensDatasource,
 		eth_etl.New(),
 		eip1577.New(s.employer),
 	}
@@ -173,7 +165,6 @@ func (s *Server) Initialize() (err error) {
 		gitcoin.New(),
 		snapshot.New(),
 		crossbell.New(),
-		lens_worker.New(),
 		transaction.New(),
 	}
 
