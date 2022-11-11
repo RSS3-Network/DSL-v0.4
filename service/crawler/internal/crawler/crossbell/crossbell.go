d239ac4e6f2d285ac045c1604d5aae5bcebebd54
polebug <polebugfly@gmail.com>
2022-11-11 18:00:31 +0800

    update(crawler): clean crossbell dirty data

--- service/crawler/internal/crawler/crossbell/crossbell.go
+++ service/crawler/internal/crawler/crossbell/crossbell.go
@@ -84,11 +84,11 @@ func (s *service) Run() error {
 			continue
 		}
 
-		// deduplicate data
-		transactions, err = database.DeduplicateTransactions(ctx, transactions)
-		if err != nil || len(transactions) == 0 {
-			continue
-		}
+		// // deduplicate data
+		// transactions, err = database.DeduplicateTransactions(ctx, transactions)
+		// if err != nil || len(transactions) == 0 {
+		// 	continue
+		// }
 
 		// build transaction
 		message := &protocol.Message{
