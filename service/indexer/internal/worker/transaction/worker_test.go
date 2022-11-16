54f55a01ea68be81763103a97acd6dcf21bcb92c
kallydev <kallydev@gmail.com>
2022-11-16 15:21:00 +0800

    ci: fix invalid testcase

--- service/indexer/internal/worker/transaction/worker_test.go
+++ service/indexer/internal/worker/transaction/worker_test.go
@@ -72,7 +72,9 @@ func Test_service_Name(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			s := tokenWorker
-			assert.Nil(t, s.Jobs())
+
+			assert.NotNil(t, s.Jobs())
+
 			if got := s.Name(); got != tt.want {
 				t.Errorf("service.Name() = %v, want %v", got, tt.want)
 			}
