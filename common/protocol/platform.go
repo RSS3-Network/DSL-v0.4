9a1ba68e8ce5b6eed9ee68db922be17bba0fc163
KallyDev <kallydev@gmail.com>
2022-12-16 06:25:18 +0800

    feat(worker/swap): cow support (#358)

--- common/protocol/platform.go
+++ common/protocol/platform.go
@@ -59,6 +59,7 @@ const (
 	PlatformTofuNFT        = "tofuNFT"
 	PlatformBlur           = "Blur"
 	PlatformElement        = "Element"
+	PlatformCow            = "Cow"
 
 	// metaverse
 	PlatformMars4      = "Mars4"
