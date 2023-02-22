aa6bbab84f39ca3c09c56956ce2c536005dfec9d
polebug <polebugfly@gmail.com>
2023-02-22 14:59:14 +0800

    fix(server): bug fix

--- service/indexer/internal/server/server.go
+++ service/indexer/internal/server/server.go
@@ -584,7 +584,7 @@ func (s *Server) handleWorkers(ctx context.Context, message *protocol.Message, t
 
 	// Sort, latest -> oldest
 	sort.SliceStable(transactions, func(i, j int) bool {
-		return transactions[i].BlockNumber < transactions[j].BlockNumber
+		return transactions[i].BlockNumber > transactions[j].BlockNumber
 	})
 
 	var (
