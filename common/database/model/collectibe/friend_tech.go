1b6450a709e0561cad65aa6481e306e49693893a
brucexc <brucexc@rss3.io>
2023-09-10 19:04:50 -0400

    fix: rename friend tech column name

--- common/database/model/collectibe/friend_tech.go
+++ common/database/model/collectibe/friend_tech.go
@@ -5,10 +5,10 @@ import "time"
 type FriendTech struct {
 	ID              int64     `gorm:"column:id;not null;primaryKey" json:"-"`
 	Address         string    `gorm:"column:address;not null;primaryKey" json:"-"`
-	TwitterUsername string    `gorm:"column:twitterUsername;not null" json:"twitterUsername"`
-	TwitterName     string    `gorm:"column:twitterName;not null" json:"twitterName"`
-	TwitterPfpUrl   string    `gorm:"column:twitterPfpUrl" json:"twitterPfpUrl"`
-	TwitterUserId   string    `gorm:"column:twitterUserId" json:"twitterUserId"`
+	TwitterUsername string    `gorm:"column:twitter_username;not null" json:"twitterUsername"`
+	TwitterName     string    `gorm:"column:twitter_name;not null" json:"twitterName"`
+	TwitterPfpUrl   string    `gorm:"column:twitter_pfp_url" json:"twitterPfpUrl"`
+	TwitterUserId   string    `gorm:"column:twitter_user_id" json:"twitterUserId"`
 	CreatedAt       time.Time `gorm:"column:created_at;autoCreateTime;not null;default:now();index" json:"-"`
 	UpdatedAt       time.Time `gorm:"column:updated_at;autoUpdateTime;not null;default:now();index" json:"-"`
 }
